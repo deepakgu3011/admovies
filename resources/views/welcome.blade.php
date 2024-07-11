e37a652f0a6c1f1eff530471c021b89170c6e57f
Deepak Kumar <deepakjct654321@outlook.com>
2024-07-11 21:10:59 +0530

    update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,55 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="text-center">Welcome to Our Film Website!</h1>
+    <div class="container">
+        <h1 class="text-center">Welcome to Our Film Website!</h1>
 
-    <h2 class="text-center">
-        <marquee behavior="alternate" direction="right">Latest Movies And webseries Available:</marquee>
-    </h2>
-    <hr>
+        <h2 class="text-center">
+            <marquee behavior="alternate" direction="right">Latest Movies And webseries Available:</marquee>
+        </h2>
+        <hr>
 
-    <h3 class="text-danger">Webseries</h3>
-    <div class="row">
-        @foreach ($movies as $movie)
-            @if ($movie->category === 'webseries')
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="{{ url($movie->pic) }}" class="card-img-top" alt="{{ $movie->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie->name }}</h5>
-                            <p class="card-text">Director: {{ $movie->dirname }}</p>
-                            <p class="card-text">Release Date: {{ $movie->rdate }}</p>
-                            <p class="card-text">{{ $movie->desc }}</p>
-                            <a href="{{ asset($movie->url) }}" class="btn btn-info">Click To Download</a>
+        <h3 class="text-danger">Webseries</h3>
+        <div class="row">
+            @foreach ($movies as $movie)
+                @if ($movie->category === 'webseries')
+                    <div class="col-lg-4 mb-4">
+                        <div class="card">
+                            <img src="{{ url($movie->pic) }}" class="card-img-top" alt="{{ $movie->name }}"
+                                style="    height: 200px;object-fit: cover;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->name }}</h5>
+                                <p class="card-text">Director: {{ $movie->dirname }}</p>
+                                <p class="card-text">Release Date: {{ $movie->rdate }}</p>
+                                <p class="card-text">{{ $movie->desc }}</p>
+                                <a href="{{ asset($movie->url) }}" class="btn btn-info">Click To Download</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
-        @endforeach
-    </div>
+                @endif
+            @endforeach
+        </div>
 
-    <hr>
+        <hr>
 
-    <h3 class="text-danger">Movies</h3>
-    <div class="row">
-        @foreach ($movies as $movie)
-            @if ($movie->category === 'movies')
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="{{ url($movie->pic) }}" class="card-img-top" alt="{{ $movie->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie->name }}</h5>
-                            <p class="card-text">Director: {{ $movie->dirname }}</p>
-                            <p class="card-text">Release Date: {{ $movie->rdate }}</p>
-                            <p class="card-text">{{ $movie->desc }}</p>
-                            <a href="{{ asset($movie->url) }}" class="btn btn-info">Click To Download</a>
+        <h3 class="text-danger">Movies</h3>
+        <div class="row">
+            @foreach ($movies as $movie)
+                @if ($movie->category === 'movies')
+                    <div class="col-lg-4 mb-4">
+                        <div class="card">
+                            <img src="{{ url($movie->pic) }}" class="card-img-top" alt="{{ $movie->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->name }}</h5>
+                                <p class="card-text">Director: {{ $movie->dirname }}</p>
+                                <p class="card-text">Release Date: {{ $movie->rdate }}</p>
+                                <p class="card-text">{{ $movie->desc }}</p>
+                                <a href="{{ asset($movie->url) }}" class="btn btn-info">Click To Download</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
-        @endforeach
-    </div>
+                @endif
+            @endforeach
+        </div>
 
-</div>
+    </div>
 @endsection
